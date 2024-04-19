f30e70b289c9ccaf775726aa451cdc6c4e5e2616
Dishankchovatia <dishankpatel03@gmail.com>
2024-04-19 17:31:10 +0530

    user login and sign up functionality and add pic using cloudinary

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,17 @@ const express = require("express");
 const  chats  = require("./data/data.js");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome");
-});
+app.use(express.json());
 
+
+app.use("/api/user", userRoutes);
 app.get("/api/chat", (req, res) => {
   res.send(chats);
 });
@@ -19,6 +21,9 @@ app.get("/api/chat/:id", (req, res) => {
   const singleChat = chats.find((c) => c._id === req.params.id);
   res.send(singleChat);
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 
 const PORT = process.env.PORT || 5000;
