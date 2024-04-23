6a07c45b8fa5e7f251daa47f5cd25eea5447ecd1
Dishankchovatia <dishankpatel03@gmail.com>
2024-04-23 16:33:04 +0530

    create messge routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const colors = require("colors");
 
@@ -21,6 +22,7 @@ app.get('/', (req, res) => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 app.get("/api/chat", (req, res) => {
   res.send(chats);
 });
